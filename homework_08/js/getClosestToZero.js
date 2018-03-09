ebfce5c1735a2761cb53724baf357cf51a594cf2
Yurii Khomyn <yuriikhomyn.lv@gmail.com>
2018-03-09 20:37:57 +0200

    Delete getClosestToZero.js

--- homework_08/js/getClosestToZero.js
+++
@@ -1,8 +0,0 @@
-function getClosestToZero(...arr){
-    for (let i = 0; i < arr.length; i++) {
-        if (Math.abs(arr[i]) < Math.abs(arr[0])) {
-            arr[0] = arr[i];
-        }
-    }
-    return arr[0];
-}
